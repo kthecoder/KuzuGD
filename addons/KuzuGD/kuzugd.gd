6ee653a98a10d39517762cf5ca5e41a613103953
kthecoder <15720121+kthecoder@users.noreply.github.com>
2025-05-31 06:02:12 -0400

    Update addons files

--- addons/KuzuGD/kuzugd.gd
+++ addons/KuzuGD/kuzugd.gd
@@ -1,8 +1,12 @@
 @tool
 extends EditorPlugin
 
-func _enter_tree():
+
+func _enter_tree() -> void:
+	# Initialization of the plugin goes here.
 	pass
 
-func _exit_tree():
+
+func _exit_tree() -> void:
+	# Clean-up of the plugin goes here.
 	pass
